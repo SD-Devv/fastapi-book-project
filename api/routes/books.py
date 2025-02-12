1d6099a6457e361800b2a53bad4e3da57268a9ea
SD-Devv <103285649+SD-Devv@users.noreply.github.com>
2025-02-12 21:08:37 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,8 +1,6 @@
-from typing import OrderedDict
-
-from fastapi import APIRouter, status
+from typing import Dict
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
-
 from api.db.schemas import Book, Genre, InMemoryDB
 
 router = APIRouter()
@@ -41,10 +39,8 @@ async def create_book(book: Book):
     )
 
 
-@router.get(
-    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
-)
-async def get_books() -> OrderedDict[int, Book]:
+@router.get("/", response_model=Dict[int, Book], status_code=status.HTTP_200_OK)
+async def get_books() -> Dict[int, Book]:
     return db.get_books()
 
 
@@ -60,3 +56,11 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.books.get(book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
